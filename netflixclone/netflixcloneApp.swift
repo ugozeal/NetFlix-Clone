eb057882ea0cff33f186e89d51e75fd481fabeba
David Okonkwo <dokonkwo@getcarbon.co>
2021-07-21 12:42:53 +0100

    Initial Commit

---
+++ netflixclone/netflixcloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflixcloneApp.swift
+//  netflixclone
+//
+//  Created by David Okonkwo on 21/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct netflixcloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
