1a061db254d5b0bfb41ef11e8ef8fe1d41c95be5
David Okonkwo <dokonkwo@getcarbon.co>
2021-07-22 17:25:30 +0100

    started the custom tabs

--- iOS/Helpers/GlobalHelpers.swift
+++ iOS/Helpers/GlobalHelpers.swift
@@ -86,3 +86,11 @@ extension LinearGradient {
         startPoint: .top,
         endPoint: .bottom)
 }
+
+extension String {
+    func widthOfString(usingFont font: UIFont) -> CGFloat {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttributes)
+        return size.width
+    }
+}
--- iOS/MovieDetail.swift
+++ iOS/MovieDetail.swift
@@ -51,6 +51,24 @@ struct MovieDetail: View {
                         
                         CastInfoView(movie: movie)
                         
+                        HStack(spacing: 60) {
+                            SmallVerticalButton(text: "My List", isOnImage: "checkmark", isOffImage: "plus", isOn: true) {
+                                //
+                            }
+                            
+                            SmallVerticalButton(text: "Rate", isOnImage: "hand.thumbsup.fill", isOffImage: "hand.thumbsup", isOn: true) {
+                                //
+                            }
+                            
+                            SmallVerticalButton(text: "Share", isOnImage: "square.and.arrow.up", isOffImage: "square.and.arrow.up", isOn: true) {
+                                //
+                            }
+                            Spacer()
+                        }
+                        .padding(.leading, 20)
+                        
+//                        CustomTabSwitcher()
+
                     }
                     .padding(.horizontal, 10)
                 }
